0df37d2333ccd19e4c70c7bdba5737672c3d0bae
ugesh <ugesh.roopchand@hotmail.com>
2018-04-23 14:19:54 +0400

    Simple DB OOP connection

--- db.php
+++ db.php
@@ -1,4 +1,22 @@
 <?php
-/*first */
+/*database connection */
+
+
+Class Database {
+
+    public $con;
+    public function __construct() {
+        $this->con = mysqli_connect ("localhost","root","1234","medicine");
+        if($this->con) {
+            echo "connected";
+        }else {
+            echo "Not connected";
+        }
+    }
+}
+
+/*Make the database object */
+
+$database = new Database;
 
 ?>
